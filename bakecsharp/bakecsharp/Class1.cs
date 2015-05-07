7c1d4a20bb3b7de4a8c451b387ebe0e7dc3683f2
Aeyzaz <aeyzaz@hotmail.com>
2015-05-07 23:38:41 +0500

    Added Methods

--- bakecsharp/bakecsharp/Class1.cs
+++ bakecsharp/bakecsharp/Class1.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace bakecsharp
 {
-    public class Class1
+    public class Bake
     {
+        static public Int32 LetterCounter(String[] x)
+        {
+            int count = 0;
+            foreach (String s in x)
+            {
+                int len = 0;
+                foreach (Char c in s)
+                {
+                    if (c != 32 && c != 45) // Not counting Space and Hyphen
+                    {
+                        len++;
+                    }
+                }
+                count += len;
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// Takes an Integer as an argument and returns an Integer array with Splitted number
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        static public Int32[] SplitNumber(Int32 num)
+        {
+            String n_org = num.ToString();
+            String[] n = new String[n_org.Length];
+            int counter = 0;
+            foreach (Char c in n_org)
+            {
+                n[counter++] = c.ToString();
+            }
+            Int32[] numbers = new Int32[n.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(n[i]);
+            }
+            return numbers;
+        }
+
     }
 }
